01e37b12b6af47e94834343805db25edb4952a5f
Anthony Jin <ajin@lcs.0n.ca>
2021-10-22 08:55:42 -0400

    Got my 3 images to show

---
+++ FavouriThings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FavouriThings
+//
+//  Created by Anthony Jin on 2021-10-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("MyRoom")
+            .resizable()
+            .scaledToFit()
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ FavouriThings/FavouriThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriThingsApp.swift
+//  FavouriThings
+//
+//  Created by Anthony Jin on 2021-10-20.
+//
+
+import SwiftUI
+
+@main
+struct FavouriThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
