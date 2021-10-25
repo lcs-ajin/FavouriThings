19bc8f536c253aec83578bb5c1cfc721f66a3431
Anthony Jin <ajin@lcs.0n.ca>
2021-10-25 12:11:14 -0400

    created a list view for our app-- this will be the landing page for our app

---
+++ FavouriThings/Views/FavouriteThingsListView.swift
@@ -0,0 +1,24 @@
+//
+//  FavouriteThingsListView.swift
+//  FavouriThings
+//
+//  Created by Anthony Jin on 2021-10-25.
+//
+
+import SwiftUI
+
+struct FavouriteThingsListView: View {
+    var body: some View {
+        List{
+        Text("MyRoom")
+        Text("Queen")
+        Text("Potato")
+    }
+}
+
+struct FavouriteThingsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouriteThingsListView()
+    }
+}
+
